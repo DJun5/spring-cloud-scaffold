7c3cf28f1eef896c999b8735bb28407bb316aa38
DJun <903131009@qq.com>
2019-07-22 14:19:53 +0800

    整合RabbitMq

--- demo-common/src/main/java/com/djun/demo/common/rabbitmq/RabbitMqConfig.java
+++ demo-common/src/main/java/com/djun/demo/common/rabbitmq/RabbitMqConfig.java
@@ -1,11 +1,18 @@
 package com.djun.demo.common.rabbitmq;
 
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 /**
  * Created by DJun on 2019/7/22.
@@ -13,79 +20,76 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class RabbitMqConfig {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public static final String EXCHANGE_A = "my-mq-exchange_A";
+    public static final String QUEUE_A = "QUEUE_A";
+    public static final String ROUTING_KEY_A = "spring-boot-routingKey_A";
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
 
     /**
-     * 订单消息实际消费队列所绑定的交换机
+     * 连接Rabbit
+     * @return 连接对象
      */
     @Bean
-    DirectExchange orderDirect() {
-        return (DirectExchange) ExchangeBuilder
-                .directExchange(QueueEnum.QUEUE_ORDER_CANCEL.getExchange())
-                .durable(true)
-                .build();
-    }
-
-    @Bean
-    ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("10.12.9.108");
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
-        connectionFactory.setPort(5672);
-        connectionFactory.setVirtualHost("/demo");
+     ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host,port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setPublisherConfirms(true);
         return connectionFactory;
     }
 
     /**
-     * 订单延迟队列队列所绑定的交换机
+     * 创建消息模板
      */
     @Bean
-    DirectExchange orderTtlDirect() {
-        return (DirectExchange) ExchangeBuilder
-                .directExchange(QueueEnum.QUEUE_TTL_ORDER_CANCEL.getExchange())
-                .durable(true)
-                .build();
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
+    }
+
+    /*
+     * 针对消费者配置
+     * 1. 设置交换机类型
+     * 2. 将队列绑定到交换机
+     *    FanoutExchange  : 将消息分发到所有的绑定队列，无routing_key的概念
+     *    HeadersExchange ：通过添加属性key-value匹配
+     *    DirectExchange  : 按照routing_key分发到指定队列
+     *    TopicExchange   :  多关键字匹配
+     */
+
+    /**
+     * 创建交换器
+     */
+    @Bean
+    public DirectExchange defaultExchange() {
+        return new DirectExchange(EXCHANGE_A);
     }
 
     /**
-     * 订单实际消费队列
+     * 创建队列A
      */
     @Bean
-    public Queue orderQueue() {
-        return new Queue(QueueEnum.QUEUE_ORDER_CANCEL.getName());
+    public Queue queueA() {
+        return new Queue(QUEUE_A, true); //队列持久
     }
 
     /**
-     * 订单延迟队列（死信队列）
+     * 使用路由键（routingKey）把队列（Queue）绑定到交换器（Exchange）
      */
     @Bean
-    public Queue orderTtlQueue() {
-        return QueueBuilder
-                .durable(QueueEnum.QUEUE_TTL_ORDER_CANCEL.getName())
-                .withArgument("x-dead-letter-exchange", QueueEnum.QUEUE_ORDER_CANCEL.getExchange())//到期后转发的交换机
-                .withArgument("x-dead-letter-routing-key", QueueEnum.QUEUE_ORDER_CANCEL.getRouteKey())//到期后转发的路由键
-                .build();
-    }
-
-    /**
-     * 将订单队列绑定到交换机
-     */
-    @Bean
-    Binding orderBinding(DirectExchange orderDirect,Queue orderQueue){
-        return BindingBuilder
-                .bind(orderQueue)
-                .to(orderDirect)
-                .with(QueueEnum.QUEUE_ORDER_CANCEL.getRouteKey());
-    }
-
-    /**
-     * 将订单延迟队列绑定到交换机
-     */
-    @Bean
-    Binding orderTtlBinding(DirectExchange orderTtlDirect,Queue orderTtlQueue){
-        return BindingBuilder
-                .bind(orderTtlQueue)
-                .to(orderTtlDirect)
-                .with(QueueEnum.QUEUE_TTL_ORDER_CANCEL.getRouteKey());
+    public Binding bindingA() {
+        return BindingBuilder.bind(queueA()).to(defaultExchange()).with(RabbitMqConfig.ROUTING_KEY_A);
     }
 
 }
--- demo-provider-8001/src/main/java/com/djun/demo/controller/DeptController.java
+++ demo-provider-8001/src/main/java/com/djun/demo/controller/DeptController.java
@@ -1,5 +1,6 @@
 package com.djun.demo.controller;
 
+
 import com.djun.demo.common.result.CommonResult;
 import com.djun.demo.deptservice.entity.Dept;
 import com.djun.demo.deptservice.service.DeptService;
@@ -21,43 +22,45 @@ public class DeptController {
     @Resource
     private DeptService service;
 
-    @ApiOperation(value="新增部门", notes="新增单个部门",produces = "application/json")
+    @ApiOperation(value = "新增部门", produces = "application/json")
     @PostMapping("/add")
-    public CommonResult add(@RequestBody Dept model){
+    public CommonResult add(@RequestBody Dept model) {
         return CommonResult.success(service.add(model));
     }
 
-    @ApiOperation(value="查询指定部门", notes="通过id查询",produces = "application/json")
+    @ApiOperation(value = "查询指定部门", notes = "通过id查询", produces = "application/json")
     @GetMapping("/findById/{id}")
 
     // 切面的方式
     @HystrixCommand(fallbackMethod = "processHystrix_Get")
-    public CommonResult get(@PathVariable("id") Long id){
-        List<Dept> dept ;
+    public CommonResult get(@PathVariable("id") Long id) {
+        List<Dept> dept;
         dept = service.get(id);
-        if (dept.size() == 0){
+        if (dept.size() == 0) {
 //            throw new RuntimeException("该ID:"+id+"，没有对应的信息");
-            return CommonResult.failed("该ID:"+id+"，没有对应的信息");
+            return CommonResult.failed("该ID:" + id + "，没有对应的信息");
         }
         return CommonResult.success(service.get(id));
     }
 
-    @ApiOperation(value="查询全部部门", notes="获取所有部门")
+    @ApiOperation(value = "查询全部部门", notes = "获取所有部门")
     @GetMapping("/list")
-    public CommonResult getAll(){
+    public CommonResult getAll() {
         return CommonResult.success(service.list());
     }
 
-    @ApiOperation(value="删除部门", notes="通过id删除")
+    @ApiOperation(value = "删除部门", notes = "通过id删除")
     @PostMapping("/delete/{id}")
-    public CommonResult delete(@PathVariable("id") Long id){
+    public CommonResult delete(@PathVariable("id") Long id) {
         return CommonResult.success(service.delete(id));
     }
 
     /**
      * 响应注解 @HystrixCommand
      */
-    public CommonResult processHystrix_Get(Long id){
-        return CommonResult.failed("该ID："+id+"，没有对应的信息--->@HystrixCommand");
+    public CommonResult processHystrix_Get(Long id) {
+        return CommonResult.failed("该ID：" + id + "，没有对应的信息--->@HystrixCommand");
     }
+
+
 }
